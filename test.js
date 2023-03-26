713e6395050738998c5756f28464249e0d841492
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-03-26 16:18:30 +0600

    test Js added

---
+++ test.js
@@ -0,0 +1,28 @@
+//console.log("I Love You Ma");
+
+// let Declare : 
+ let = 7;
+if(true){
+
+  let x = 6;
+   console.log(x);
+
+ }
+ console.log(x); 
+
+// Var Declare :
+carName = "Volvo";
+var carName;
+
+console.log(carName);
+
+
+// const Declare : 
+const cars = ["Saab", "Volvo", "BMW"];
+
+ 
+cars[0] = "Toyota";
+
+ 
+cars.push("Audi");
+console.log(cars);
