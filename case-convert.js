a6f10ee12eb006723362f037710caecffd2f97a4
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-03-27 15:51:05 +0600

    Array slice &inchTofeet & moremap program added

---
+++ case-convert.js
@@ -0,0 +1,13 @@
+ // Hazrat Ali
+// University Of Scholars
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+
+
+const userInput = 'blACkPinK';
+const savedUserName = 'blackPink';
+// console.log(userInput.toLowerCase());
+// console.log(savedUserName.toLowerCase());
+if (userInput.toLowerCase() == savedUserName.toLowerCase()) {
+    console.log('user exists');
+}
