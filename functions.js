b4520f9ea4f75b6a376a096a30cbfb19a5d5f1be
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-03-27 15:15:42 +0600

    Nested condition & Concatination program added

---
+++ functions.js
@@ -0,0 +1,14 @@
+// function game (name , time ){
+
+//  console.log(name + " is playing from " + time );
+
+//  game("hazrat", "10 pm");
+
+
+// }
+
+let x = myFunction(4, 3);    
+
+function myFunction(a, b) {
+  return a * b;              
+}
--- test.js
+++ test.js
@@ -26,3 +26,33 @@ cars[0] = "Toyota";
  
 cars.push("Audi");
 console.log(cars);
+
+//let x = 5 + 5;  //Type Of 
+//let y = "5" + 5;
+//let z = "Hello" + 5;
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+// DataTypes : 
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
