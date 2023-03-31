f85bdadf8331b645c0908609f20ee45f9373121c
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-03-31 15:39:50 +0600

    Object Property & Null Undefined program added

---
+++ arguments.js
@@ -0,0 +1,8 @@
+// Hazrat Ali
+// University Of Scholars
+function add(num1, num2) {
+  console.log([...arguments]);
+  return num1 + num2 + arguments[2];
+}
+const result = add(2, 3, 5, 7);
+console.log(result);
---
+++ double-vs-triple-equal.js
@@ -0,0 +1,10 @@
+// Hazrat Ali
+// University Of Scholars
+const first = 0;
+const second = false;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
