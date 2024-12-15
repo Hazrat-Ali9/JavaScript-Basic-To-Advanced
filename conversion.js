fffb145de2e97dbfa1ee6fcb4b7be2f45ce3b377
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-15 11:59:02 +0600

    conversation  :tulip:

--- conversion.js
+++ conversion.js
@@ -4,7 +4,7 @@ var price1 = "31";
 var price2 = "10";
 var totalPrice = price1 + price2;
 // console.log(totalPrice);
-
+// Conversation
 var price1 = 31;
 var price2 = "10";
 var totalPrice = price1 + price2;
