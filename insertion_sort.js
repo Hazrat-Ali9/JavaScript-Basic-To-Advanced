5511b39172ddbd420c59bc5bc7dbaa57d737907f
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-16 10:31:44 +0600

    insertion sort :watch:

--- insertion_sort.js
+++ insertion_sort.js
@@ -14,7 +14,7 @@ function insertionSort(array) {
     }
     return array;
 }
-
+// insertion sort
 console.log(insertionSort([7,5,2,4,3,9])); 
 console.log(insertionSort([9,7,5,4,3,1])); 
 console.log(insertionSort([1,2,3,4,5,6]));
