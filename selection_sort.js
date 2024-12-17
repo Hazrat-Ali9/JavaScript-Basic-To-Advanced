5c637a207b40918b0a0a457cf6c3d8825b7f937f
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-17 21:28:37 +0600

    Selection sort :lemon:

--- selection_sort.js
+++ selection_sort.js
@@ -5,7 +5,7 @@ const numbers  = [12, 54, 1, 67, -5, 9, 76, 17];
 // [-5, 1, 54, 67, 12, 9, 76, 17]
 // [-5, 1, 9, 67, 12, 54, 76, 17]
 // [-5, 1, 9, 12, 67, 54, 76, 17]
-
+// Selection Sort
 function selectionSort(array){
     for (let i = 0; i < array.length; i++) {
         let min = i;
