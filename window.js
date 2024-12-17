d730edd2606d72934fd0fd71ca268ef625715942
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-17 21:56:10 +0600

    Windows js 🤠

--- window.js
+++ window.js
@@ -10,7 +10,7 @@ function add (num1, num2){
     var total = double(result);
     return total;
 }
-// Window js
+// Window jS
 // console.log('name outside', name);
 //console.log('result outside', result);
 var sum = add(13, 21);
