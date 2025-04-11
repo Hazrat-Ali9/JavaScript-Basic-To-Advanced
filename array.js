13aedea4a2372f034de2556ad813f201f7d9d089
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-04-11 22:31:22 +0600

    array js 🧔‍♂️

--- array.js
+++ array.js
@@ -10,8 +10,8 @@ var oddNumbers = [1, 3, 5, 7, 9];
 
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 
-// console.log(nayikas.length);
+// console.log(nayikas.length); 
 var vowelsCount = vowels.length;
 console.log(vowelsCount)
 
-// array js
+// Array js
