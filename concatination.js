6c5efa659225d56db6ed16b546e441fd90158d93
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-16 10:08:57 +0600

    Concatination  :bulb:

--- concatination.js
+++ concatination.js
@@ -4,8 +4,8 @@ var price1 = 31;
 var price2 = 10;
 var totalPrice = price1 + price2;
 // console.log(totalPrice);
-var firstName = 'Jennie';
-var lastName = 'Kim';
+var firstName = 'Hazrat';
+var lastName = 'Ali';
 var fullName = firstName + ' ' + lastName;
 console.log(fullName);
 
